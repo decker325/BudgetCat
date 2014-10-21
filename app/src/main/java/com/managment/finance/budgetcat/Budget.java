f7232629bd14ec40269ed76d3dd9c32266690723
decker325 <mdecker.d325@gmail.com>
2014-10-20 20:23:19 -0400

    commit
    commit

---
+++ app/src/main/java/com/managment/finance/budgetcat/Budget.java
@@ -0,0 +1,15 @@
+package com.managment.finance.budgetcat;
+
+/**
+ * Created by Mychelle on 10/20/2014.
+ */
+
+public class Budget {
+
+    private int budgetID;
+
+    public Budget(){
+
+    }
+
+}
